6528c3f7430d09781b5be0822b1dffe019916b5d
Yunseong <dev.yunseong@gmail.com>
2026-04-27 21:39:03 +0900

    Add QUBO utility functions for binary polynomials

---
+++ mr2s_module/util/__init__.py
@@ -0,0 +1,11 @@
+from mr2s_module.util.qubo_util import (
+  get_indicator_function,
+  multiply_polys,
+  add_polys
+)
+
+__all__ = [
+  "get_indicator_function",
+  "multiply_polys",
+  "add_polys"
+]
