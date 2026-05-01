7fb765b3a25ca16d6a61decb406ae7b37ba06331
Yunseong <dev.yunseong@gmail.com>
2026-05-01 11:17:45 +0900

    feat: add QuboMR2SSolver and additional classes to module exports

--- mr2s_module/evaluator/__init__.py
+++ mr2s_module/evaluator/__init__.py
@@ -1,3 +1,4 @@
 from mr2s_module.evaluator.evaluator import Evaluator
+from mr2s_module.evaluator.apsp_sum_evaluator import ApspSumEvaluator
 
-__all__ = ["Evaluator"]
+__all__ = ["Evaluator", "ApspSumEvaluator"]
